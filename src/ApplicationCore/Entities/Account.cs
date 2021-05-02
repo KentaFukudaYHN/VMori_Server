ef00c6e123fdb5fac1b733b58186f6ac6825a135
wsl$\Ubuntu\home <yhn_18782@icloud.com>
2021-05-02 11:22:05 +0900

    アカウントテーブルの定義

---
+++ src/ApplicationCore/Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationCore.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    /// <summary>
+    /// アカウント情報
+    /// </summary>
+    public class Account : BaseEntity
+    {
+        /// <summary>
+        /// ニックネーム
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// メール
+        /// </summary>
+        public string Mail { get; set; }
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// アイコン
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        /// 性別
+        /// </summary>
+        public GenderKinds Gender { get; set; }
+        /// <summary>
+        /// 生年月日 YYYYMMdd
+        /// </summary>
+        public string Birthday { get; set; }
+        /// <summary>
+        /// 登録日時
+        /// </summary>
+        public DateTime RegistDateTime { get; set; }
+    }
+}
--- src/ApplicationCore/Enums.cs
+++ src/ApplicationCore/Enums.cs
@@ -1,7 +1,4 @@
-﻿using System;
-
-
-namespace ApplicationCore.Enum
+﻿namespace ApplicationCore.Enum
 {
     /// <summary>
     /// 動画プラットフォームの種類
@@ -12,5 +9,28 @@ namespace ApplicationCore.Enum
         NikoNiko = 20,
         Twitch = 30,
         TwitchCasting = 40,
+    }
+
+    /// <summary>
+    /// 性別
+    /// </summary>
+    public enum GenderKinds
+    {
+        /// <summary>
+        /// 不明、無回答
+        /// </summary>
+        UnKnown = 0,
+        /// <summary>
+        /// 男性
+        /// </summary>
+        Male = 100,
+        /// <summary>
+        /// 女性
+        /// </summary>
+        Female = 200,
+        /// <summary>
+        /// その他
+        /// </summary>
+        Other = 300,
     }
 }
