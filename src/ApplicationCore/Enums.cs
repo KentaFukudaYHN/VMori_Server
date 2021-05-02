d1911e84bde73963272630e39c1fe55d40652c25
wsl$\Ubuntu\home <yhn_18782@icloud.com>
2021-05-02 13:17:52 +0900

    アカウントテーブルの定義

--- src/ApplicationCore/Enums.cs
+++ src/ApplicationCore/Enums.cs
@@ -13,4 +13,24 @@ namespace ApplicationCore.Enum
         Twitch = 30,
         TwitchCasting = 40,
     }
+
+    public enum GenderKinds
+    {
+        /// <summary>
+        /// 不明・未回答
+        /// </summary>
+        UnKnown = 0,
+        /// <summary>
+        /// 男性
+        /// </summary>
+        Male = 100,
+        /// <summary>
+        /// 女性
+        /// </summary>
+        Female = 200,
+        /// <summary>
+        /// その他
+        /// </summary>
+        Other = 300
+    }
 }
---
+++ src/Infrastructure/Data/VMoriContext.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    /// <summary>
+    /// VMoriDbContext
+    /// </summary>
+    public class VMoriContext : DbContext
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="options"></param>
+        public VMoriContext(DbContextOptions options) : base(options){}
+
+        /// <summary>
+        /// 動画情報
+        /// </summary>
+        public DbSet<VideoInfo> VideoInfos { get; set; }
+        
+        /// <summary>
+        /// アカウント情報
+        /// </summary>
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VideoInfo>().ToTable("VideoInfo");
+            modelBuilder.Entity<Account>().ToTable("Account");
+        }
+    }
+}
